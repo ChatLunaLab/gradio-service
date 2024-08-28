9d35c2255fc072ade53a8d714e14183cda17a9f4
dingyi <dingyi222666@foxmail.com>
2024-08-29 02:47:37 +0800

    feat: init commit

---
+++ readme.md
@@ -0,0 +1,22 @@
+<div align="center">
+
+# koishi-plugin-gradio-service
+
+_提供 @gradio/client 支持_
+
+## [![npm](https://img.shields.io/npm/v/koishi-plugin-gradio-service)](https://www.npmjs.com/package/koishi-plugin-gradio-service) [![npm](https://img.shields.io/npm/dm/koishi-plugin-gradio-service)](https://www.npmjs.com/package/koishi-plugin-gradio-service) ![node version](https://img.shields.io/badge/node-%3E=18-green) ![github top language](https://img.shields.io/github/languages/top/ChatLunaLab/chatluna-character?logo=github)
+
+</div>
+
+## 特性
+
+无
+
+## 使用
+
+```typescript
+import type {  } from "koishi-plugin-gradio-service"
+
+const app = ctx.gradio.connect(".....")
+
+```
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable max-len */
+import { Context, Schema, Service } from 'koishi'
+import { Client } from '@gradio/client'
+
+export * from '@gradio/client'
+class GradioClientService extends Service {
+    constructor(ctx: Context) {
+        super(ctx, 'gradio')
+    }
+
+    connect(...args: Parameters<(typeof Client)['connect']>) {
+        return Client.connect(...args)
+    }
+
+    duplicate(...args: Parameters<(typeof Client)['duplicate']>) {
+        return Client.duplicate(...args)
+    }
+}
+
+export const inject = {}
+
+namespace GradioClientService {
+    export interface Config {}
+
+    export const Config = Schema.object({})
+
+    export const name = 'gradio-service'
+}
+
+export default GradioClientService
